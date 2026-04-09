913aea384a32a41ca209f777f37787c93e2a8381
Petr Král <kral.petr.88@gmail.com>
2026-04-09 07:24:04 +0200

    Revert "feat(skill): add scripts and references to merge-github-pr skill (#251)"
    This reverts commit a643b8355a38e356819b654e62e93e7d125e246a.

--- rector.php
+++ rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php81\Rector\ClassMethod\NewInInitializerRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -14,7 +13,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->skip([
         __DIR__ . '/vendor',
         __DIR__ . '/src/ComposerPlugin.php',
-        NewInInitializerRector::class,
     ]);
 
     $rectorConfig->import(__DIR__ . '/vendor/pekral/rector-rules/rector.php');
